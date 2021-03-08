6da27cb471ecd9e5f92c776afc9c01df2e353478
unsky559 <6kostya@gmail.com>
2021-03-08 23:25:49 +0200

    feat: extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  if(typeof str === 'undefined')
+    throw new Error('No string input')
+  if(typeof options.repeatTimes === 'undefined')
+    options.repeatTimes = 1
+  if(typeof options.separator === 'undefined')
+    options.separator = '+'
+  if(typeof options.additionSeparator === 'undefined')
+    options.additionSeparator = '|'
+  if(typeof options.additionRepeatTimes === 'undefined')
+    options.additionRepeatTimes = 1
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let result = '';
+  for(let i = 0; i < options.repeatTimes; i++){
+    result += String(str);
+    if(typeof options.addition !== 'undefined'){
+      for(let j = 0; j < options.additionRepeatTimes; j++){
+        result += String(options.addition)
+        if(j != options.additionRepeatTimes-1)
+          result += String(options.additionSeparator)
+      }
+    }
+    if(i != options.repeatTimes-1)
+      result += String(options.separator)
+  }
+  return result
 };
-  
